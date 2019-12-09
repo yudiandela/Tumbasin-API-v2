04696bd3234cc1f6f7d80921ccf67be75ab5bae9
Yudi Andela <yudhi.andhela@gmail.com>
2019-12-09 08:59:54 +0700

    CRUD Market data API

---
+++ app/Http/Controllers/Api/MarketController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\MarketResource;
+use App\Market;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class MarketController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $markets = Market::latest()->paginate();
+        return MarketResource::collection($markets);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255', 'unique:markets']
+        ]);
+
+        $market = Market::create([
+            'name' => $request->name,
+            'slug' => Str::slug($request->name)
+        ]);
+
+        return new MarketResource($market);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Market  $market
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Market $market)
+    {
+        return new MarketResource($market);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Market  $market
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Market $market)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255', 'unique:markets,name,' . $market->id]
+        ]);
+
+        $market->name = $request->name;
+        $market->slug = Str::slug($request->name);
+
+        return new MarketResource($market);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Market  $market
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Market $market)
+    {
+        $market->delete();
+        return response()->json([
+            'message' => 'Data was deleted!'
+        ], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -7,5 +7,12 @@ Route::namespace('Api')->name('api.')->prefix('v2')->middleware('api')->group(fu
     Route::middleware('auth:api')->group(function () {
         Route::post('logout', 'LogoutController')->name('logout');
         Route::get('profile', 'ProfileController')->name('profile');
+
+        // Market
+        Route::get('market', 'MarketController@index')->name('market.index');
+        Route::post('market', 'MarketController@store')->name('market.store');
+        Route::get('market/{market}', 'MarketController@show')->name('market.show');
+        Route::put('market/{market}', 'MarketController@update')->name('market.update');
+        Route::delete('market/{market}', 'MarketController@destroy')->name('market.destroy');
     });
 });
